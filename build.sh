8e2b9d5f02018588004ba82c28ae8740884b2dc4
Mark Benschop <mark.benschop@rws.nl>
2018-10-03 08:33:31 +0200

    added build.sh, script that builds, and pushes new container

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+    echo 'Please specify a version number'
+fi
+version=$1
+echo "version : ${version} "
+docker build -t flask-demo:${version}  . && \
+docker tag flask-demo:${version} markbenschop/flask-demo:${version}  && \
+docker push markbenschop/flask-demo:${version}
